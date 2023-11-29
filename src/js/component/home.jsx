957720ecb61f292c24d0997784363ac7196d97a8
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2023-11-29 17:09:18 +0000

    Semaforo con funciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,62 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [red, setRed] = useState("secondary")
+	const [yellow, setYellow] = useState("secondary")
+	const [green, setGreen] = useState("secondary")
+
+	// Prende el semaforo en rojo
+	function rojo (){
+		setRed("danger")
+		setGreen("secondary")
+		setYellow("secondary")
+	}
+
+	// Prende el semaforo en amarillo
+	function amarillo (){
+		setRed("secondary")
+		setGreen("secondary")
+		setYellow("warning")
+	}
+
+	// Prende el semaforo en verde
+	function verde (){
+		setRed("secondary")
+		setGreen("success")
+		setYellow("secondary")
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">Semáforo</h1>
+			<br />
+			<div className="">
+				{/* Rojo */}
+				<div className={"bg-" + red + " rounded-circle"}
+					style={{ height: "100px", width: "100px", marginLeft: "600px", marginBottom: "7px" }}
+					onClick={rojo}>
+				</div>
+
+				{/* Amarillo */}
+				<div className={"bg-" + yellow + " rounded-circle"}
+					style={{ height: "100px", width: "100px", marginLeft: "600px", marginBottom: "7px" }}
+					onClick={amarillo}>
+				</div>
+
+				{/* Verde */}
+				<div className={"bg-" + green + " rounded-circle"}
+					style={{ height: "100px", width: "100px", marginLeft: "600px", marginBottom: "7px" }}
+					onClick={verde}>
+				</div>
+			</div>
+
+			<div>
+				<br />
+
+				Made by 🩷 Cecilia Perdomo, with 💛 love!
+			</div>
+
 		</div>
 	);
 };
